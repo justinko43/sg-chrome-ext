0858f7571459469bc1ff01f7beeed8081e1e4f10
justin.ko43 <justin.ko43@gmail.com>
2018-05-29 12:45:41 -0700

    Add animation and abstract screenshare function out

--- speak-genius.js
+++ speak-genius.js
@@ -100,11 +100,10 @@ window.onload = function(e) {
   
   document.getElementById('speakgenius-modal').style.display = 'none';
 
-  var menuEl = document.querySelector('#test')
-  var menu = new mdc.menu.MDCMenu(menuEl);
-  var menuButtonEl = document.querySelector('#menu-button');
+  const menuEl = document.querySelector('#sg-troubleshootMenu')
+  const menu = new mdc.menu.MDCMenu(menuEl);
+  const menuButtonEl = document.querySelector('#menu-button');
 
-  // menu.setMaxHeight('100px');
   // Toggle menu open
   menuButtonEl.addEventListener('click', function() {
     menu.open = !menu.open;
@@ -120,22 +119,17 @@ window.onload = function(e) {
 }
 
 function inLesson() {
+  //turn off welcome modal, turn on speakgenius modal
   document.getElementById('speakgenius-modal').style.display = 'flex';
   document.getElementById('welcome-modal').style.display = 'none';
-
+  //display screenshare and suggested time
   document.getElementById('sg-screenShare').style.display = 'flex';
   document.getElementById('sg-suggestedTime').style.display = 'flex';
   document.getElementById('sg-instructions').textContent = inLessonDetails[i].instructions;
+
+  setScreenShare(inLessonDetails, i);
   
   document.getElementById('sg-suggestedTimeText').textContent = inLessonDetails[i].suggestedTime;
-  if (inLessonDetails[i].screenShare) {
-    document.getElementById('sg-screenShareText').textContent = 'ON';
-    document.getElementById('sg-screenShareText').style.color = 'green';
-  }
-  else {
-    document.getElementById('sg-screenShareText').textContent = 'OFF';
-    document.getElementById('sg-screenShareText').style.color = 'red';
-  }
   
   document.getElementById('left').onclick = function() {
     i = leftButtonHandler(inLessonDetails, i);
@@ -145,12 +139,27 @@ function inLesson() {
   }
 }
 
+function setScreenShare(details, index) {
+    if (details[index].screenShare) {
+    document.getElementById('sg-screenShareText').textContent = 'ON';
+    document.getElementById('sg-screenShareText').style.color = 'green';
+  }
+  else {
+    document.getElementById('sg-screenShareText').textContent = 'OFF';
+    document.getElementById('sg-screenShareText').style.color = 'red';
+  }
+}
+
 function preLessonButton() {
+  document.getElementById('sg-instructions').classList.remove('fadeOutRight');
+  document.getElementById('sg-instructions').classList.add('fadeInRight');
   preLesson();
   preLessonBool = true;
 }
 
 function inLessonButton() {
+  document.getElementById('sg-instructions').classList.remove('fadeOutRight');
+  document.getElementById('sg-instructions').classList.add('fadeInRight');
   inLesson();
   preLessonBool = false;
 }
@@ -175,19 +184,21 @@ function preLesson() {
 
 function leftButtonHandler(details, index) {
   let text = document.getElementById('sg-instructions');
-  console.log(index);
+  
+  text.classList.remove('fadeInLeft');
+  text.classList.remove('fadeInRight');
+  text.classList.add('fadeOutRight');
+
   if (index > 0) {
     index -= 1;
+    window.setTimeout(() => {
+      text.classList.remove('fadeOutRight');
+      text.classList.add('fadeInLeft');
+      text.textContent = details[index].instructions;
+    }, 300);
     text.textContent = details[index].instructions;
     document.getElementById('sg-suggestedTimeText').textContent = details[index].suggestedTime;
-    if (details[index].screenShare) {
-      document.getElementById('sg-screenShareText').textContent = 'ON';
-      document.getElementById('sg-screenShareText').style.color = 'green';
-    }
-    else {
-      document.getElementById('sg-screenShareText').textContent = 'OFF';
-      document.getElementById('sg-screenShareText').style.color = 'red';
-    }
+    setScreenShare(details, index);
   } else {
     document.getElementById('speakgenius-modal').style.display = 'none';
     document.getElementById('welcome-modal').style.display = 'flex';
@@ -197,19 +208,18 @@ function leftButtonHandler(details, index) {
 
 function rightButtonHandler(details, index) {
   let text = document.getElementById('sg-instructions');
-  console.log(index, details.length - 1);
+  text.classList.remove('fadeInLeft');
+  text.classList.remove('fadeInRight');
+  text.classList.add('fadeOutLeft');
   if (index < details.length - 1) {
     index += 1;
-    text.textContent = details[index].instructions;
+    window.setTimeout(() => {
+      text.classList.remove('fadeOutLeft');
+      text.classList.add('fadeInRight');
+      text.textContent = details[index].instructions;
+    }, 300);
     document.getElementById('sg-suggestedTimeText').textContent = details[index].suggestedTime;
-    if (details[index].screenShare) {
-      document.getElementById('sg-screenShareText').textContent = 'ON';
-      document.getElementById('sg-screenShareText').style.color = 'green';
-    }
-    else {
-      document.getElementById('sg-screenShareText').textContent = 'OFF';
-      document.getElementById('sg-screenShareText').style.color = 'red';
-    }
+    setScreenShare(details, index);
   }
   return index;
 }
