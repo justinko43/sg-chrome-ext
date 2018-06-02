10ba04c1c8b2cb2b134d6f577ce8f04188a46517
justin.ko43 <justin.ko43@gmail.com>
2018-06-02 14:12:30 -0700

    Add content changes + set instruction changes

--- speak-genius.js
+++ speak-genius.js
@@ -4,120 +4,164 @@ TODO:
 */
 const preLessonDetails = [
   { 
-    instructions: 'Organize your lesson windows', 
-    subjectMatter: 'Hello',
+    instructions1: 'Organize your lesson windows',
     screenShare: null, 
     suggestedTime: null,
     link: 'https://bit.ly/2JowRNX',
   },
   { 
-    instructions: 'Use SG’s dedicated Meet account to join your lesson', 
-    subjectMatter: 'Hello',
+    instructions1: 'Use SG’s dedicated Meet account to join your lesson',
     screenShare: null, 
     suggestedTime: null,
     link: 'https://bit.ly/2LbZPh6',
   },
   { 
-    instructions: 'Optimize your network connection settings', 
-    subjectMatter: 'Hello',
+    instructions1: 'Optimize your Meet connection settings',
     screenShare: null, 
     suggestedTime: null,
     link: 'https://bit.ly/2Lb3viZ',
   },
   { 
-    instructions: 'Keys to moderating a good lesson', 
-    subjectMatter: 'Hello',
+    instructions1: 'Keys to moderating a good lesson',
     screenShare: null, 
     suggestedTime: null,
     link: 'https://bit.ly/2JjXxPO',
   },
   { 
-    instructions: 'You’re all set! Click “Join Meeting” in Google Meet and Click the Next Arrow above =D', 
-    subjectMatter: 'End',
+    instructions1: [{ word:'You\'re all set!', color: '#598be9' }],
+    instructions2: [{word:' '}],
+    instructions3: ['I. ', { word:'Click', color: 'orange' }, '“Join Meeting” in Meet'],
+    instructions4: ['II. ', { word:'Click', color: 'orange' }, 'the Next Arrow below'],
     screenShare: null, 
     suggestedTime: null,
-    link: 'https://www.google.com/',
   }
 ]
 
 const inLessonDetails = [
   { 
-    instructions: 'Ask me anything', 
-    subjectMatter: 'Hello',
+    instructions1: '- Begin Recording: “Settings”, “Record Meeting”',
+    instructions2: ['- If you\'ve never met before, briefly', { word: 'introduce', underline: true}, 'your background and', {word:'ask', underline:true}, 'learner to do the same'],
+    subjectMatter: 'Learner Joins',
     screenShare: false, 
-    suggestedTime: 5 ,
+    suggestedTime: '<1' ,
     link: 'https://www.google.com/',
   },
   { 
-    instructions: 'Introduce the topic', 
-    subjectMatter: 'Hello',
-    screenShare: true, 
-    suggestedTime: 15,
-    link: 'https://www.google.com/',
-  },
-  { 
-    instructions: 'Have learner read the briefing', 
-    subjectMatter: 'Hello',
-    screenShare: true, 
-    suggestedTime: 15,
-    link: 'https://www.google.com/',
-  },
-  { 
-    instructions: 'Underline and go over mispronouned words', 
-    subjectMatter: 'Hello',
-    screenShare: true, 
-    suggestedTime: 15,
-    link: 'https://www.google.com/',
-  },
-  { 
-    instructions: 'Ask the lead-in question and unpack if necessary', 
-    subjectMatter: 'Hello',
-    screenShare: true, 
-    suggestedTime: 15,
-    link: 'https://www.google.com/',
-  },
-  { 
-    instructions: 'Go through lead-in inquiry', 
-    subjectMatter: 'Hello',
+    instructions1: 'Warm-up by casually exchanging questions',
+    instructions2: [{word:'Answer', underline: true}, 'your own questions', {word: 'following', underline:true}, 'your learner\'s responses'],
+    instructions3: ['eg. "Did you do anything interesting today?",', {word: '"Do you have any questions for me?"', underline: true}, '"What did you think about the pre-lesson video?"'],
+    subjectMatter: 'Ask Me Anything',
     screenShare: false, 
-    suggestedTime: 15,
+    suggestedTime: '<15',
     link: 'https://www.google.com/',
   },
   { 
-    instructions: 'Don\'t forget to write down constructive feedback!', 
-    subjectMatter: 'Hello',
+    instructions1: 'Throw in a brief comment that you’re transitioning',
+    instructions2: 'eg. “Okay cool, you did a great job with Ask Me Anything - are you ready to move onto the Lesson Topic?”',
+    subjectMatter: 'Section Transition',
     screenShare: false, 
-    suggestedTime: 15,
+    suggestedTime: 1,
     link: 'https://www.google.com/',
   },
   { 
-    instructions: 'Go over constructive feedback', 
-    subjectMatter: 'Hello',
+    instructions1: ['Be a conversational guide,', {word:'not a language teacher', underline:true}],
+    instructions2: ['Approach your learner as', {word: 'a friend who speaks native English', underline: true}],
+    instructions3: [{word: 'Aim to spend zero time', underline: true}, 'explaining English grammar / technicalities'],
+    subjectMatter: 'Friendly Reminders',
     screenShare: true, 
     suggestedTime: 15,
     link: 'https://www.google.com/',
   },
   { 
-    instructions: 'Introduce key phrase', 
-    subjectMatter: 'Hello',
+    instructions1: 'Screen Share ON: “Present Now”, “A Window”',
+    instructions2: 'Simply paraphrase the Context in your own words ',
+    instructions3: 'eg. “Today’s lesson is about [ Topic ] - we’ll be thinking about questions like, [ Context Questions ]. Ready to get started?”',
+    subjectMatter: 'Introduce the Topic',
     screenShare: true, 
-    suggestedTime: 10,
+    suggestedTime: '<1 min',
     link: 'https://www.google.com/',
   },
   { 
-    instructions: 'Go through key phrase inquiry', 
-    subjectMatter: 'Hello',
+    instructions1: 'Ask learner to read Briefing out loud',
+    instructions2: 'Underline any mis-pronounced words in real-time',
+    subjectMatter: 'Learner Reads Briefing',
+    screenShare: true, 
+    suggestedTime: 2,
+    link: 'https://www.google.com/',
+  },
+  { 
+    instructions1: 'Ask learner to repeat mis-pronounced words after you (2-3x per word)',
+    instructions2: 'Ask learner if they have any questions about the Briefing',
+    instructions3: 'eg. “Great job on reading the Briefing. We’re going to quickly review words you could have pronounced better…[ review words ]...did you have any questions before we move on?”',
+    subjectMatter: 'Review Briefing',
+    screenShare: true, 
+    suggestedTime: 2,
+    link: 'https://www.google.com/',
+  },
+  { 
+    instructions1: 'Read Lead-In and ask learner what he/she thinks',
+    instructions2: 'If learner doesn’t understand, use your judgement to unpack / review',
+    instructions3: 'eg. “Okay, so the Lead-In for today is [ Lead-In ]. If you understand the Lead-In, it’d be great to hear your thoughts.”',
+    instructions4: 'Screen Share OFF: “Stop Presenting”',
+    subjectMatter: 'Lead-In',
+    screenShare: true, 
+    suggestedTime: 2,
+    link: 'https://www.google.com/',
+  },
+  { 
+    instructions1: 'Step 1: use SG’s pre-set questions to guide your discussion ',
+    instructions2: 'Step 2: paraphrase learner’s response and (if relevant) follow up with your own brief commentary ',
+    instructions3: 'Step 3: dig deeper with your own follow up questions (eg. “Why?”) ',
+    instructions4: 'eg. “Interesting, so what you’re saying is [ key takeaway ]...[ follow up commentary ]...[ follow up question ]...[ SG’s pre-set question ]”',
+    subjectMatter: 'Inquiry',
     screenShare: false, 
-    suggestedTime: 10,
+    suggestedTime: '<10',
     link: 'https://www.google.com/',
   },
   { 
-    instructions: 'End the lesson', 
-    subjectMatter: 'Hello',
+    instructions1: 'Screen Share ON',
+    instructions2: 'Briefly read all written Constructive Feedback',
+    instructions3: 'Ask your learner if he/she has any questions on your statements',
+    instructions4: 'eg. “Before we move on, I’d just like to quickly review a few statements I corrected while you were speaking…[ Simply Read Statements ]”',
+    subjectMatter: 'Review Constructive Feedback',
+    screenShare: true, 
+    suggestedTime: 1,
+    link: 'https://www.google.com/',
+  },
+  { 
+    instructions1: 'eg. “If you’re ready, let’s move onto today’s Key Phrase?”',
+    subjectMatter: 'Section Transition',
     screenShare: false, 
     suggestedTime: 0,
     link: 'https://www.google.com/',
-  }
+  },
+  { 
+    instructions1: 'Step 1: read the key phrase and sample sentence out loud',
+    instructions2: 'Step 2: ask your learner what he/she thinks the word means',
+    instructions3: 'Step 3: recap the meaning in your own words',
+    instructions4: 'eg. “The Key Phrase for today is [ Key Phrase ]...in a Sample Sentence [ Sample Sentence ]...Based on the sentence, what do you think the phrase means?...[ recap meaning ]”',
+    subjectMatter: 'Key Phrase',
+    screenShare: true, 
+    suggestedTime: 2,
+    link: 'https://www.google.com/',
+  },
+  { 
+    instructions1: 'Screen Share OFF',
+    instructions2: 'Use SG’s pre-set questions to guide your discussion',
+    instructions3: 'eg. “Interesting, so what you’re saying is [ key takeaway ]...[ follow up commentary ]...[ follow up question ]...[ SG’s pre-set question ] ”',
+    subjectMatter: 'Inquiry',
+    screenShare: false, 
+    suggestedTime: '5-10',
+    link: 'https://www.google.com/',
+  },
+  { 
+    instructions1: 'If 35 minutes has not passed, ask your learner if they have any questions for you ',
+    instructions2: 'End the session with a positive comment about your learner’s performance',
+    subjectMatter: 'Closing the Session',
+    screenShare: false, 
+    suggestedTime: 1,
+    link: 'https://www.google.com/',
+  },
 ];
 
 let i = 0;
@@ -189,15 +233,15 @@ function inLesson() {
   //turn off welcome modal, turn on speakgenius modal
   document.getElementById('speakgenius-modal').style.display = 'flex';
   document.getElementById('welcome-modal').style.display = 'none';
+  document.getElementById('sg-documentation').style.display = 'none';
   document.getElementById('demo-menu').style.display = 'block';
   document.getElementById('sg-category').style.display = 'block';
   //display screenshare and suggested time
   document.getElementById('sg-screenShare').style.display = 'flex';
   document.getElementById('sg-suggestedTime').style.display = 'flex';
-  document.getElementById('sg-instructions').textContent = inLessonDetails[i].instructions;
-  document.getElementById('sg-category').textContent = inLessonDetails[i].subjectMatter;
   document.getElementById('sg-documentation').href = inLessonDetails[i].link;
-
+  //sg instructions
+  setInstructions(inLessonDetails, i);
   setScreenShare(inLessonDetails, i);
   
   document.getElementById('sg-suggestedTimeText').textContent = inLessonDetails[i].suggestedTime + ' min';
@@ -208,6 +252,75 @@ function inLesson() {
   document.getElementById('right').onclick = function() {
     i = rightButtonHandler(inLessonDetails, i);
   }
+}
+
+function addDynamicText(inputArray) {
+  let myText = document.createElement('p');
+  inputArray.forEach((element) => {
+    if (typeof element === 'object') {
+      let textNode = document.createTextNode(element.word);
+      let node;
+      if (element.italics && element.underline) {
+        node = document.createElement('U');
+        node.appendChild(textNode);
+        node.style.fontStyle = 'italic';
+      } else if (element.italics) {
+        node = document.createElement('I');
+        node.appendChild(textNode);
+      } else if (element.underline) {
+        node = document.createElement('U');
+        node.appendChild(textNode);
+      } else {
+        node = document.createElement('span');
+        node.appendChild(textNode);
+      }
+      if (element.color) node.style.color = element.color;
+      myText.appendChild(node);
+      myText.append(" ");
+    } else {
+      let textNode = document.createTextNode(element);
+      myText.appendChild(textNode);
+      myText.append(" ");
+    }
+  })
+  return myText;
+}
+
+function setInstructions(details, index) {
+  if (details[index].instructions1) {
+    document.getElementById('sg-instructions1').style.display = 'flex';
+    if (Array.isArray(details[index].instructions1)) {
+      if (document.getElementById('sg-instructions1').hasChildNodes()) document.getElementById('sg-instructions1').removeChild(document.getElementById('sg-instructions1').firstChild);
+      document.getElementById('sg-instructions1').appendChild(addDynamicText(details[index].instructions1));
+    } else document.getElementById('sg-instructions1').textContent = details[index].instructions1;
+  } else document.getElementById('sg-instructions1').style.display = 'none';
+  if (details[index].instructions2) {
+    console.log('hello')
+    let instruction2 = document.getElementById('sg-instructions2');
+    instruction2.style.display = 'flex';
+    if (Array.isArray(details[index].instructions2)) {
+      if (instruction2.hasChildNodes()) instruction2.removeChild(instruction2.firstChild);
+      instruction2.appendChild(addDynamicText(details[index].instructions2));
+    } else document.getElementById('sg-instructions2').textContent = details[index].instructions2;
+  } else document.getElementById('sg-instructions2').style.display = 'none';
+  if (details[index].instructions3) {
+    let instruction3 = document.getElementById('sg-instructions3')
+    instruction3.style.display = 'flex';
+    if (Array.isArray(details[index].instructions3)) {
+      if (instruction3.hasChildNodes()) instruction3.removeChild(instruction3.firstChild);
+      instruction3.appendChild(addDynamicText(details[index].instructions3));
+    } else document.getElementById('sg-instructions3').textContent = details[index].instructions3;
+  } else document.getElementById('sg-instructions3').style.display = 'none';
+  if (details[index].instructions4) {
+    let instruction4 = document.getElementById('sg-instructions4');
+    instruction4.style.display = 'flex';
+    if (Array.isArray(details[index].instructions4)) {
+      if (instruction4.hasChildNodes()) instruction4.removeChild(instruction4.firstChild);
+      instruction4.appendChild(addDynamicText(details[index].instructions4));
+    } else document.getElementById('sg-instructions4').textContent = details[index].instructions4;
+  } else document.getElementById('sg-instructions4').style.display = 'none';
+
+  if (details[index].subjectMatter) document.getElementById('sg-category').textContent = details[index].subjectMatter;
 }
 
 function setScreenShare(details, index) {
@@ -222,11 +335,13 @@ function setScreenShare(details, index) {
 }
 
 function preLessonButton() {
+  f = 0;
   preLesson();
   preLessonBool = true;
 }
 
 function inLessonButton() {
+  i = 0;
   inLesson();
   preLessonBool = false;
 }
@@ -237,12 +352,13 @@ function preLesson() {
   document.getElementById('demo-menu').style.display = 'none';
   document.getElementById('sg-category').style.display = 'none';
 
-  document.getElementById('sg-category').textContent = preLessonDetails[f].subjectMatter;
-  document.getElementById('sg-instructions').textContent = preLessonDetails[f].instructions;
+  document.getElementById('sg-documentation').style.display = 'flex';
   document.getElementById('sg-documentation').href = preLessonDetails[f].link;
-
+  
   document.getElementById('sg-screenShare').style.display = 'none';
   document.getElementById('sg-suggestedTime').style.display = 'none';
+  
+  setInstructions(preLessonDetails, f);
 
   document.getElementById('left').onclick = function() {
     f = leftButtonHandler(preLessonDetails, f);
@@ -254,14 +370,14 @@ function preLesson() {
 }
 
 function leftButtonHandler(details, index) {
-  let text = document.getElementById('sg-instructions');
   if (index > 0) {
+    if (index === details.length - 1 && preLessonBool) document.getElementById('sg-documentation').style.display = 'flex';
     index -= 1;
     if (!preLessonBool) document.getElementById('sg-category').textContent = details[index].subjectMatter;
-    text.textContent = details[index].instructions;
     document.getElementById('sg-documentation').href = details[index].link;
     document.getElementById('sg-suggestedTimeText').textContent = details[index].suggestedTime + ' min';
     setScreenShare(details, index);
+    setInstructions(details, index);
   } else {
     document.getElementById('speakgenius-modal').style.display = 'none';
     document.getElementById('welcome-modal').style.display = 'flex';
@@ -270,14 +386,14 @@ function leftButtonHandler(details, index) {
 }
 
 function rightButtonHandler(details, index) {
-  let text = document.getElementById('sg-instructions');
   if (index < details.length - 1) {
     index += 1;
-    if (!preLessonBool) document.getElementById('sg-category').textContent = details[index].subjectMatter;    
-    text.textContent = details[index].instructions;
+    if (!preLessonBool) document.getElementById('sg-category').textContent = details[index].subjectMatter;
+    if (index === details.length - 1 && preLessonBool) document.getElementById('sg-documentation').style.display = 'none';
     document.getElementById('sg-documentation').href = details[index].link;
     document.getElementById('sg-suggestedTimeText').textContent = details[index].suggestedTime + ' min';
     setScreenShare(details, index);
+    setInstructions(details, index);
   } else {
     if (preLessonBool) {
       inLesson();
